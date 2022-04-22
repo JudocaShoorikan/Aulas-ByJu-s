ed4ec6f296dd41fbd3e5f99ec5996f72e13a8605
JudocaShoorikan <104225117+JudocaShoorikan@users.noreply.github.com>
2022-04-22 16:06:45 -0300

    quadradinho vermelho be like

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var bola
+function setup() {
+  createCanvas(400,400);
+bola = createSprite(200,200,50,50);
+bola.shapeColor = "red";
+}
+
+function draw() 
+{
+  background(30);
+drawSprites();
+}
+
+
+
+
