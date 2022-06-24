69c363e23ba764b263ef84ea2cbb6b3836451c51
JudocaShoorikan <thisbranadc@gmail.com>
2022-06-24 16:17:17 -0300

    Aula C18 Student group

--- Aula C18/sketch.js
+++ Aula C18/sketch.js
@@ -1,11 +1,14 @@
+var student;
 
 function setup() {
   createCanvas(400,400);
-
+  student = new Student('Thiago', '12', 'CAMB', 'São Paulo', "7º ano");
+  student.show()
 }
 
 function draw() 
 {
   background(30);
-  
+
 }
+
--- Aula C18/student.js
+++ Aula C18/student.js
@@ -1,7 +1,20 @@
 
 class Student
 {
-   
+    constructor(nome, idade, escola, cidade, serie) {
+        this.student = {
+            nome, 
+            idade,
+            escola,
+            cidade,
+            serie,
+        }
+    }
+
+    show() {
+        console.log(this.student.nome, this.student.idade, this.student.escola, this.student.cidade, this.student.serie);
+    }
+    
 }
 
 
